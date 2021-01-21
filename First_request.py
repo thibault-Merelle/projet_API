3b6c6ed9a5793ebb80b0043f873b04e90278243c
Thibault_Merelle <merellethibault@gmail.com>
2021-01-22 00:01:17 +0100

    mise en place fichier CSV

--- First_request.py
+++ First_request.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import json
+import csv
 import requests
 import logging
 from pprint import pprint 
@@ -17,9 +18,9 @@ r = requests.get('https://api.sncf.com/v1/coverage/sncf/stop_areas', auth = ('75
 if r.status_code == 200:
     print('Success!')
     logging.info("status code 200")
-elif r.status_code == 404:
+elif r.status_code != 200:
     print('not Found.')
-    logging.warning("status code 404")   
+    logging.warning("status code {r.status_code}")   
 
 with open("api_sncf.json", "w") as API:
     json.dump(r.text, API)
@@ -28,10 +29,41 @@ with open("api_sncf.json", "w") as API:
 pprint(r.json())
 
 
+#fichier csv:
 
+try:
+    #reader
+    with open('gares_sncf.csv', 'r') as csv_file:
+            csv_reader = csv.reader(csv_file) #delimiter à développer
+            logging.info("csv Reader ok")
+    #        next(csv_reader)
+    #        for line in csv_reader:
+    #            print(line)
+    #avec writer :
+            with open('gares_sncf.csv', 'w') as new_file:
+                csv_writer = csv.writer(new_file, delimiter='\t') 
+                logging.info("csv Writer ok")
 
+                for line in csv_reader:
+                    csv_writer.writerow(line) #acces row [i]
+
+except:
+    logging.warning("Erreur fichier Data CSV") 
     
+#avec dict.writer
+#keys = header => plus simple à lire et partager
+'''
+with open('gares_sncf.csv', 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
 
-#myJson = r.json()
-#myJson_formatted = json.dump(myJson, indent=4)
+    with open ('gares_sncf', 'w') as new_file:
+        fieldnames = ['AAAA', 'BBBB', 'CCCCC']
 
+        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter='\t')
+        csv.writer.writeheader()
+
+            for line in csv_reader:
+                #del line['CCCCC'] #delete colomn 'CCCC'
+                csv.writer.writerow(line)
+ '''
+        
